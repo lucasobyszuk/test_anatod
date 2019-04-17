5ae53d818196216374dea854b8efc2cd39e9feec
lucasobyszuk <lucasobyszuk@gmail.com>
2019-04-17 16:19:57 -0300

    Separated new/edit client view on new file and litle changes on index and script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
- $(document).ready(function(){
-    $("button[name='newClientButton']").click(function(){
-    	if ($(".form-cliente").css("display") == "none") {
-    		$(".form-cliente").css("display","block");
-    	}
-    });
-  });
+$("button[name='newClientButton']").click(function(){
+  $("#main-section").load("clientform.html");
+});
